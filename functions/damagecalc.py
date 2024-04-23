ae2d88ed7d2f240308f200606cbbbe0ddb465c6d
Kyrnnlord <krynnlord@yahoo.com>
2024-04-23 16:58:29 -0500

    Improved the damage roll. Made universal instead of just hero

--- functions/damagecalc.py
+++ functions/damagecalc.py
@@ -1,48 +1,53 @@
 import random
 
-
-def hero_damage_calc(hero_stats, hero_weapon):
-    
-    # Attack Value is weapon attack +- 25%
-    atk_value = random.randrange(round(hero_weapon - (hero_weapon * .25)), round(hero_weapon + (hero_weapon * .25)))
-    
-    # Initilize Modifier
-    modifier_value = 0
-    
-    # Initilize Crit (0-No 1-Yes)
-    hero_crit = 0
-    
-    # Player Luck chance for items. Sets luck value - 20
-    luckmod = random.randrange(hero_stats.luck, 20)
-    if luckmod >= 16:
-
-        # create 3d6 for roll
-        modrand1 = (random.randrange(1,6))
-        modrand2 = (random.randrange(1,6))
-        modrand3 = (random.randrange(1,6))
-        modrand = max(modrand1,modrand2,modrand3)
-
-        # Sets Crit Modified value
-        modifier_value = round((atk_value + (modrand +2)))
-        
-        # Set Crit to Yes
-        hero_crit = 1 
-
-    else:
-        # No Crit just normal attack
-        modifier_value = atk_value 
-
-    # return value and crit value
-    return modifier_value, hero_crit 
-
-# New Dice Roller Takes in string value template like 1d4
+###########################################################
+# New Dice Roller Takes in string value template like 1d4 #
+###########################################################
 def diceroller(diceroll):
-    
-    total = []
+	
+	total = []
 
-    for i in range(int(diceroll[0])):
-        roll = random.randrange(1, int(diceroll[2])+1)
-        total.append(roll)
-        final = max(total)
-    
-    return final
+	for i in range(int(diceroll[0])):
+		roll = random.randrange(1, int(diceroll[2])+1)
+		total.append(roll)
+		final = max(total)
+	
+	return final
+
+#################################################################
+# Crit Chance roller. Dice must be 20 to crit. Improved by Luck #
+#################################################################
+def critroller(luck):
+	
+	crit = 0
+	roll = random.randrange((1 + luck), 21)
+
+	if roll == 20:
+		crit = 1
+	else:
+		crit = 0
+	return crit
+
+##########################
+# Main Damage Calculator #
+##########################
+def damage_calc(luck, weapon_damage):
+	
+	# Base Attack Value
+	atk_value = diceroller(weapon_damage)
+	
+	# Initilize Modifier and Crit(0-No 1-Yes)
+	modifier_value = 0
+	success_crit = 0
+	
+	# Roll Crit Dice
+	success_crit = critroller(luck)
+	
+	# Result in crit or no crit
+	if success_crit == 1:
+		modifier_value = atk_value + diceroller(weapon_damage)
+	else:
+		modifier_value = atk_value 
+
+	# return value and crit value
+	return modifier_value, success_crit 
